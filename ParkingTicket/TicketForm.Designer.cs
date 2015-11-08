0d742b011f6f2d763ffeeb53a90e327be14db35d
Shane <shane8641@live.com>
2015-11-08 09:54:26 -0500

    added default constructor

---
+++ ParkingTicket/TicketForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ParkingTicket
+{
+    partial class TicketForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTicketTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTicketMake = new System.Windows.Forms.TextBox();
+            this.textBoxTicketModel = new System.Windows.Forms.TextBox();
+            this.textBoxTicketColor = new System.Windows.Forms.TextBox();
+            this.textBoxTicketPlate = new System.Windows.Forms.TextBox();
+            this.textBoxTicketFine = new System.Windows.Forms.TextBox();
+            this.textBoxTicketCop = new System.Windows.Forms.TextBox();
+            this.textBoxTicketBadge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelTicketTitle
+            // 
+            this.labelTicketTitle.AutoSize = true;
+            this.labelTicketTitle.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTicketTitle.Location = new System.Drawing.Point(155, 9);
+            this.labelTicketTitle.Name = "labelTicketTitle";
+            this.labelTicketTitle.Size = new System.Drawing.Size(159, 30);
+            this.labelTicketTitle.TabIndex = 1;
+            this.labelTicketTitle.Text = "Parking Ticket";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Make:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Model:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Color:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Plate #:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Total Fine:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Officer Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 397);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Badge #:";
+            // 
+            // textBoxTicketMake
+            // 
+            this.textBoxTicketMake.Location = new System.Drawing.Point(118, 94);
+            this.textBoxTicketMake.Name = "textBoxTicketMake";
+            this.textBoxTicketMake.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketMake.TabIndex = 13;
+            // 
+            // textBoxTicketModel
+            // 
+            this.textBoxTicketModel.Location = new System.Drawing.Point(118, 146);
+            this.textBoxTicketModel.Name = "textBoxTicketModel";
+            this.textBoxTicketModel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketModel.TabIndex = 14;
+            // 
+            // textBoxTicketColor
+            // 
+            this.textBoxTicketColor.Location = new System.Drawing.Point(118, 189);
+            this.textBoxTicketColor.Name = "textBoxTicketColor";
+            this.textBoxTicketColor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketColor.TabIndex = 15;
+            // 
+            // textBoxTicketPlate
+            // 
+            this.textBoxTicketPlate.Location = new System.Drawing.Point(118, 239);
+            this.textBoxTicketPlate.Name = "textBoxTicketPlate";
+            this.textBoxTicketPlate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketPlate.TabIndex = 16;
+            // 
+            // textBoxTicketFine
+            // 
+            this.textBoxTicketFine.Location = new System.Drawing.Point(118, 292);
+            this.textBoxTicketFine.Name = "textBoxTicketFine";
+            this.textBoxTicketFine.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketFine.TabIndex = 17;
+            // 
+            // textBoxTicketCop
+            // 
+            this.textBoxTicketCop.Location = new System.Drawing.Point(118, 347);
+            this.textBoxTicketCop.Name = "textBoxTicketCop";
+            this.textBoxTicketCop.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketCop.TabIndex = 18;
+            // 
+            // textBoxTicketBadge
+            // 
+            this.textBoxTicketBadge.Location = new System.Drawing.Point(118, 397);
+            this.textBoxTicketBadge.Name = "textBoxTicketBadge";
+            this.textBoxTicketBadge.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicketBadge.TabIndex = 19;
+            // 
+            // TicketForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(512, 505);
+            this.Controls.Add(this.textBoxTicketBadge);
+            this.Controls.Add(this.textBoxTicketCop);
+            this.Controls.Add(this.textBoxTicketFine);
+            this.Controls.Add(this.textBoxTicketPlate);
+            this.Controls.Add(this.textBoxTicketColor);
+            this.Controls.Add(this.textBoxTicketModel);
+            this.Controls.Add(this.textBoxTicketMake);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelTicketTitle);
+            this.Name = "TicketForm";
+            this.Text = "TicketForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTicketTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTicketMake;
+        private System.Windows.Forms.TextBox textBoxTicketModel;
+        private System.Windows.Forms.TextBox textBoxTicketColor;
+        private System.Windows.Forms.TextBox textBoxTicketPlate;
+        private System.Windows.Forms.TextBox textBoxTicketFine;
+        private System.Windows.Forms.TextBox textBoxTicketCop;
+        private System.Windows.Forms.TextBox textBoxTicketBadge;
+    }
+}
